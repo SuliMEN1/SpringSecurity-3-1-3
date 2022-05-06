f937b0902bb6fdb95fc7d9850263b683e77a7873
SuliMEN1 <suli-my@mail.ru>
2022-05-06 17:46:03 +0300

    js begin

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/controller/RegistrationController.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/controller/RegistrationController.java
@@ -1,18 +1,16 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+
+@Controller
 public class RegistrationController {
 
-//    @PostMapping("/login")
-//    public String loginPage(@RequestBody User user) {
-//        return "login";
-//    }
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
 }
--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -4,12 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserDetailsServiceImpl;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api")
 public class UserRestController {
     private final UserDetailsServiceImpl userDetailsService;
 
